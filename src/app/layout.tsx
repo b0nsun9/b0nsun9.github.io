86e1ff37a435dd86c6794f32ee96560311f638f0
Bonsung Koo <developer@bonsung.me>
2024-11-25 08:59:24 +1300

    Migrated page router to app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "On The Code",
+  description: "Bonsung's blog",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
